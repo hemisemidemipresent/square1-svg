f439f610634d4c36fef311161d17f894819471ba
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-12-29 10:13:23 +0800

    Update e.js

--- e.js
+++ e.js
@@ -1,24 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-
-let shape = [2, 1, 2, 1, 2, 1, 2, 1];
-app.get('/:algorithm', (req, res) => {
-    const Square1 = require('./square1');
-    let alg = req.query.algorithm;
-    if (!alg) alg = '';
-    if (alg.length > 200) return res.send('input algorithm too long');
-    console.log(alg);
-    try {
-        let sq = new Square1(alg);
-        res.setHeader('Content-Type', 'image/svg+xml');
-        res.send(sq.toSVG());
-    } catch (e) {
-        console.log(e);
-        res.send('invalid algorithm');
-    }
-});
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`);
-});
+const Square1 = require('./square1');
+let sq = new Square1('/(0,-3)');
+console.log(sq.toSVG());
